8d079ce881cc777bdc9626f7d6a8273a94e557f0
yotu <114195789+ulxsth@users.noreply.github.com>
2023-09-13 09:28:53 +0900

    init

---
+++ POLICE_WANTED/src/main/java/com/ulxsth/police_wanted/POLICE_WANTED.java
@@ -0,0 +1,17 @@
+package com.ulxsth.police_wanted;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class POLICE_WANTED extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
