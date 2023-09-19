23cdf069ca75ca20696a44d58754bce63121d462
yotu <114195789+ulxsth@users.noreply.github.com>
2023-09-19 16:25:56 +0900

    refactor: rename

--- POLICE_WANTED/src/main/java/com/ulxsth/police_wanted/PoliceWantedPlugin.java
+++ POLICE_WANTED/src/main/java/com/ulxsth/police_wanted/PoliceWantedPlugin.java
@@ -2,9 +2,7 @@ package com.ulxsth.police_wanted;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
-
-public final class POLICE_WANTED extends JavaPlugin {
+public final class PoliceWantedPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
