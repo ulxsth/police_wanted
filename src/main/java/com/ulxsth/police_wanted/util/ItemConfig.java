e5f8e44fe926cf43e3701ac03620134f10704607
yotu <114195789+ulxsth@users.noreply.github.com>
2023-09-25 04:36:40 +0900

    add: item.ymlのデータを操作するクラスを作成

---
+++ src/main/java/com/ulxsth/police_wanted/util/ItemConfig.java
@@ -0,0 +1,36 @@
+package com.ulxsth.police_wanted.util;
+
+import com.ulxsth.police_wanted.PoliceWantedPlugin;
+import com.ulxsth.police_wanted.model.item.GameItem;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+public class ItemConfig {
+    private static final ItemConfig instance = new ItemConfig();
+
+    private static final PoliceWantedPlugin plugin = PoliceWantedPlugin.getInstance();
+    public static final String PATH = plugin.getDataFolder().getPath() + "/item.yml";
+
+    private static final YamlConfiguration config = YamlConfiguration.loadConfiguration(new File(PATH));
+
+    private ItemConfig() {}
+
+    public static ItemConfig getInstance() {
+        return instance;
+    }
+
+    public static GameItem getItemById(String id) {
+        ConfigurationSection section = config.getConfigurationSection("id");
+        if(section == null) {
+            throw new IllegalArgumentException("id is invalid");
+        }
+
+        String materialId = section.getString(id + ".material");
+        String name = section.getString(id + ".name");
+        int itemId = section.getInt(id + ".id");
+
+        return new GameItem(materialId, name, itemId);
+    }
+}
