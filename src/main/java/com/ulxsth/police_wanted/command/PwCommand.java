e66f6ad864280514912a4091a681ffb736e02244
yotu <114195789+ulxsth@users.noreply.github.com>
2023-09-25 05:29:14 +0900

    update: giveを追加

--- src/main/java/com/ulxsth/police_wanted/command/PwCommand.java
+++ src/main/java/com/ulxsth/police_wanted/command/PwCommand.java
@@ -3,15 +3,22 @@ package com.ulxsth.police_wanted.command;
 import com.ulxsth.police_wanted.PoliceWantedPlugin;
 import com.ulxsth.police_wanted.scheduler.GameTimerScheduler;
 import com.ulxsth.police_wanted.usecase.BroadcastMessageUseCase;
+import com.ulxsth.police_wanted.usecase.CreateItemStackUseCase;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.logging.Logger;
 
 public class PwCommand implements CommandExecutor {
     private static final String COMMAND_HELP = "USAGE:\n" +
             "/pw start <time> : ゲームを開始します\n" +
-            "/pw stop : ゲームを終了します\n";
+            "/pw stop : ゲームを終了します\n" +
+            "/pw give <item> <amount> : アイテムを入手します\n" +
+            "/pw give <item> <amount> <player> : 指定したプレイヤーにアイテムを入手させます\n";
 
     private static final PoliceWantedPlugin plugin = PoliceWantedPlugin.getInstance();
     GameTimerScheduler gameTimerScheduler = null;
@@ -57,6 +64,44 @@ public class PwCommand implements CommandExecutor {
             this.gameTimerScheduler = null;
 
             BroadcastMessageUseCase.execute("ゲームを終了しました");
+            return true;
+        }
+
+        // give
+        if(args[0].equalsIgnoreCase("give")) {
+            // 引数が足りない
+            if(args.length < 2) {
+                sender.sendMessage(COMMAND_HELP);
+                return true;
+            }
+
+            // 引数が数字でない
+            if(!args[2].matches("^[0-9]+$")) {
+                sender.sendMessage("個数は数字で指定してください");
+                return true;
+            }
+
+            // アイテムを渡すプレイヤーを取得（指定がなければコマンド実行者）
+            Player target = args.length == 4 ? plugin.getServer().getPlayer(args[2]) : (Player) sender;
+            if(target == null) {
+                sender.sendMessage("指定したプレイヤーは存在しません");
+                return true;
+            }
+
+            // アイテムを渡す
+            ItemStack itemStack = null;
+            try {
+                CreateItemStackUseCase.execute(args[1], Integer.parseInt(args[2]));
+            } catch (IllegalArgumentException e) {
+                sender.sendMessage("指定したidのアイテムは存在しません");
+                plugin.logger.info(e.getMessage());
+                return true;
+            }
+
+            target.getInventory().addItem(itemStack);
+
+            // 指定したプレイヤーにアイテムを入手させる
+            sender.sendMessage("アイテムを渡しました");
             return true;
         }
 
