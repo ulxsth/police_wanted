e7599a2da805852d8e12e72b9d0de6aaf2bae62c
yotu <114195789+ulxsth@users.noreply.github.com>
2023-09-25 06:07:39 +0900

    fix: configの初期化を追加

--- src/main/java/com/ulxsth/police_wanted/util/ItemConfig.java
+++ src/main/java/com/ulxsth/police_wanted/util/ItemConfig.java
@@ -8,28 +8,30 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import java.io.File;
 
 public class ItemConfig {
-    private static final ItemConfig instance = new ItemConfig();
-
     private static final PoliceWantedPlugin plugin = PoliceWantedPlugin.getInstance();
     public static final String PATH = plugin.getDataFolder().getPath() + "/item.yml";
 
-    private static final YamlConfiguration config = YamlConfiguration.loadConfiguration(new File(PATH));
+    private static YamlConfiguration config;
 
     private ItemConfig() {}
 
-    public static ItemConfig getInstance() {
-        return instance;
+    public static void initConfig() {
+        if(!new File(PATH).exists()) {
+            plugin.saveResource("item.yml", false);
+             config = YamlConfiguration.loadConfiguration(new File(PATH));
+        }
     }
 
     public static GameItem getItemById(String id) {
+        initConfig();
         ConfigurationSection section = config.getConfigurationSection(id);
         if(section == null) {
             throw new IllegalArgumentException("cant find: " + id);
         }
 
-        String materialId = section.getString(id + ".material");
-        String name = section.getString(id + ".name");
-        int itemId = section.getInt(id + ".id");
+        String materialId = section.getString("material");
+        String name = section.getString("name");
+        int itemId = section.getInt("id");
 
         return new GameItem(materialId, name, itemId);
     }
